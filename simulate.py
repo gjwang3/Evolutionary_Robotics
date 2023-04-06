f779840910fdede178630c2fd50b5b58697390e1
gjwang3 <109115274+gjwang3@users.noreply.github.com>
2023-04-06 12:30:56 -0700

    Add files via upload

--- simulate.py
+++ simulate.py
@@ -8,6 +8,10 @@ import time
 physicsClient = p.connect(p.GUI)
 # disable the sidebars on the pybullet simulation/speed up simulation
 #p.configureDebugVisualizer(p.COV_ENABLE_GUI,0)
+
+# tells pybullet to read in the world described in box.sdf.
+p.loadSDF("box.sdf")
+
 for i in range(1000):
   p.stepSimulation()
 #sleeping our code by 1/60th of a second during each pass through the loop
