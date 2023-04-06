c90c397556bf9e68a713f11717e09ae5716647f5
gjwang3 <109115274+gjwang3@users.noreply.github.com>
2023-04-05 17:12:27 -0700

    Create simulate.py

---
+++ simulate.py
@@ -0,0 +1,20 @@
+# run this code in terminal; otherwise some functions do not work in Spyder
+
+#test if phybullet is installed
+import pybullet as p
+import time
+
+#creat an object 
+physicsClient = p.connect(p.GUI)
+# disable the sidebars on the pybullet simulation/speed up simulation
+#p.configureDebugVisualizer(p.COV_ENABLE_GUI,0)
+for i in range(1000):
+  p.stepSimulation()
+#sleeping our code by 1/60th of a second during each pass through the loop
+  time.sleep(1/60)
+  print(i)
+  
+p.disconnect()
+
+
+
